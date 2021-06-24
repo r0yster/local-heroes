31cabbd0afa282c1b1eed24910d1eda4f13cf71e
Lexie Crawford <lexcraw4d@gmail.com>
2021-06-24 16:55:40 -0400

    added models and user table functionality to work with sequelize

--- app.js
+++ app.js
@@ -1,43 +1,31 @@
+require('dotenv').config();
+
 const express = require('express');
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const sequelize = require('./config/connection');
 const path = require('path');
 const app = express();
 const cookieParser = require('cookie-parser');
 
-dotenv.config({
-	path: './.env',
-});
+const pagesRouter = require('./routes/pages');
 
-const db = mysql.createConnection({
-	host: process.env.DATABASE_HOST,
-	user: process.env.DATABASE_USER,
-	password: process.env.DATABASE_PW,
-	database: process.env.DATABASE,
-});
+const PORT = process.env.PORT || 3001;
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
 //Parsing url encoded bodies (as sent by html forms)
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.set('view engine', 'hbs');
 
-db.connect((error) => {
-	if (error) {
-		console.log(error);
-	} else {
-		console.log('My SQL connected...');
-	}
-});
-
 //Define Routes
+// app.use(express.static(path.join(__dirname, '/views')));
+app.get('/', (req, res) => res.render('index.hbs'));
 app.use('/', require('./routes/pages'));
-app.use('/auth', require('./routes/auth'))
+app.use('/auth', require('./routes/auth'));
 
-
-
-app.listen(3001);
-console.log('Server started on Port 3001');
+sequelize.sync({ force: false }).then(() => {
+	console.log(`Sequelize connected to MySQL successfully.`);
+	app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
+});
--- routes/pages.js
+++ routes/pages.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     //renders handlebars index
-    res.render('index')
+    res.render('/index')
 });
 router.get('/login', (req, res) => {
     //renders handlebars login
@@ -20,5 +20,6 @@ router.get('/userOptions', (req, res) => {
     //renders handlebars registration
     res.render('userOptions')
 });
+
 
 module.exports = router;
