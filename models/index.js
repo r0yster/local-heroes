59ff96ee1108099f9d0379678411721cea9b618e
royster <rcantu786@gmail.com>
2021-06-26 11:49:13 -0500

    finalized associations

--- models/index.js
+++ models/index.js
@@ -3,8 +3,60 @@ const Category = require('./Category');
 const Location = require('./Location');
 const Venue = require('./Venue');
 
-Venue.belongsTo(Category, {
+User.belongsTo(Location, {
+    foreignKey: 'id'
+});
+
+Location.hasMany(User, {
+    foreignKey: 'id'
+});
+
+User.belongsToMany(Category, {
+    through: UserInterests,
+    as: 'interests',
+    foreignKey: 'id'
+});
+
+Category.belongsToMany(User, {
+    through: UserInterests,
+    as: 'category',
+    foreignKey: 'id'
+});
+
+User.belongsToMany(Venue, {
+    through: UserVenues,
+    as: 'venue_list',
+    foreignKey: 'id'
+});
+
+Venue.belongsToMany(User, {
+    through: UserVenues,
+    as: 'visted_users',
+    foreignKey: 'id'
+});
+
+Venue.belongsToMany(Category, {
+    through: VenueCategory,
+    as: 'category',
+    foreignKey: 'id'
+});
+
+Category.belongsToMany(Venue, {
+    through: VenueCategory,
+    as: 'category',
+    foreignKey: 'id'
+});
+
+Venue.belongsToMany(Location, {
+    through: VenueLocation,
+    as: 'venue',
     foreignKey: 'category'
-})
+});
+
+Location.belongsToMany(Venue, {
+    through: VenueLocation,
+    as: 'city',
+    foreignKey: 'id'
+});
 
 module.exports = { User, Category, Location, Venue };
