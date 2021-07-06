09bf896e7f92daf3348c2e91ed71aa67891e7316
Lex Crawford <lexcraw4d@gmail.com>
2021-07-06 19:51:46 -0400

    added for deploy

---
+++ config/connection.js
@@ -0,0 +1,7 @@
+const Sequelize = require('sequelize');
+
+ const sequelize = process.env.JAWSDB_URL
+ 	? new Sequelize(process.env.JAWSDB_URL)
+ 	: new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+ 			host: process.env.DB_HOST || 'localhost',
+ 			dialect: 'mysql',
